cf90b6c2808a0f4879ae3d1586cef58ef6855519
gwenchang <g2chang@ucsd.edu>
2018-03-03 14:09:10 -0800

    Update Header.js

--- temp/src/components/common/Header.js
+++ temp/src/components/common/Header.js
@@ -1,22 +1,19 @@
 import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
-import LoadingDots from './LoadingDots';
 
-const Header = ({loading}) => {
-  return (
-    <nav>
-      <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      {" | "}
-      <Link to="/courses" activeClassName="active">Courses</Link>
-      {" | "}
-      <Link to="/about" activeClassName="active">About</Link>
-      {loading && <LoadingDots interval={100} dots={20}/>}
-    </nav>
-  );
-};
+const Header = React.createClass({
+  render(){
+    return(
+      <div className="menu">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/courses">Courses</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </div>
+    )
+  }
 
-Header.propTypes = {
-  loading: PropTypes.bool.isRequired
-};
+})
 
 export default Header;
