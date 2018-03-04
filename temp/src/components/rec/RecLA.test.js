ea08a9598b99a910cd4f5e6adfe54b4b878d7267
gwenchang <g2chang@ucsd.edu>
2018-03-04 12:40:33 -0800

    first test!

---
+++ temp/src/components/rec/RecLA.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import expect from 'expect';
+import {mount, shallow} from 'enzyme';
+import RecLAPage from '../rec/RecLAPage';
+
+function setup(){
+    const props ={
+        name: "",
+        description: "",
+        list: []
+    };
+    return shallow(<RecLAPage{...props}/>);
+}
+
+it('renders page', () => {
+    const wrapper = setup();
+    expect(wrapper.find('form').length).toBe(1);
+    expect(wrapper.find('h2').text()).toEqual('Post your recommendations!');
+}
+);
+
+it('Should not post empty recommendations', () =>{
+    const wrapper = setup();
+    const submitButton = wrapper.find('input').last();
+    submitButton.simulate('click');
+    expect(wrapper.state().list.length).toBe(0);
+});
