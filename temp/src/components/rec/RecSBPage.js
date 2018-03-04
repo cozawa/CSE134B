7094a94987b5d1ed485dbfbaa2f622b33da472d0
eonoprie <eonoprie@ucsd.edu>
2018-03-04 11:27:53 -0800

    Add files via upload

---
+++ temp/src/components/rec/RecSBPage.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class RecSBPage extends React.Component {
+
+constructor(props) {
+    super(props);
+    this.state = {
+        name: '',
+        description: '',
+        list: []
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+handleChange(event) {
+    const name = event.target.name;
+    this.setState({
+        [name]: event.target.value
+    });
+}
+
+handleSubmit(event) {
+    event.preventDefault();
+    let temp = this.state.list.slice();
+    temp.push(<Recpost
+        name={this.state.name}
+        description={this.state.description}
+    />);
+    this.setState({list:temp});
+}
+
+render() {
+return (
+<tbody>
+<h1>Thinking about traveling in Santa Barbara?</h1>
+
+<ul> {this.state.list.map(function(listValue){
+    return <li> {listValue} </li>;
+})} </ul>
+
+<h2>Post your recommendations!</h2>
+
+<form>
+    <label>
+        Name:
+        <input
+            name="name"
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+        />
+    </label>
+    <label>
+        Description:
+        <input
+            name="description"
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+        />
+    </label>
+    <button onClick={this.handleSubmit}>
+        Submit
+    </button>
+</form>
+</tbody>
+);}}
+
+function Recpost(props) {
+    return (
+        <div>
+        <div className="name"> {props.name} </div>
+        <div className="text"> {props.description} </div>
+        </div>
+    );
+}
+
+export default RecSBPage;
