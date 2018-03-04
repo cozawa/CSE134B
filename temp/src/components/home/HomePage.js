872d2083fd3a0ed743cc4162d514c0048328723a
eonoprie <eonoprie@ucsd.edu>
2018-03-04 11:10:26 -0800

    Add files via upload

--- temp/src/components/home/HomePage.js
+++ temp/src/components/home/HomePage.js
@@ -4,11 +4,48 @@ import {Link} from 'react-router';
 class HomePage extends React.Component {
   render() {
     return (
-      <div className="jumbotron">
-        <h1>Pluralsight Administration</h1>
-        <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
-        <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
-      </div>
+<tbody>
+
+<h1>Where to?</h1>
+
+<div className="float">
+<img src={"../images/sd.jpg"}/>
+<div className="hr">
+<p>San Diego</p>
+<Link to="/recsd">Recommendations</Link>
+<Link to="/housesd">Housing</Link>
+</div>
+</div>
+
+<div className="float">
+<img src={"../images/sb.jpg"}/>
+<div className="hr">
+<p>Santa Barbara</p>
+<Link to="/recsb">Recommendations</Link>
+<Link to="/housesb">Housing</Link>
+</div>
+</div>
+
+<div className="float">
+<img src={"../images/la.jpg"}/>
+<div className="hr">
+<p>Los Angeles</p>
+<Link to="/recla">Recommendations</Link>
+<Link to="/housela">Housing</Link>
+</div>
+</div>
+
+<div className="float">
+<img src={"../images/sf.jpg"}/>
+<div className="hr">
+<p>San Francisco</p>
+<Link to="/recsf">Recommendations</Link>
+<Link to="/housesf">Housing</Link>
+</div>
+</div>
+<br></br><br></br><br></br><br></br><br></br>
+
+</tbody>
     );
   }
 }
